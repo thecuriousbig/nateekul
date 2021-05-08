25fdd538d660fb3c7028b7c4d075611e97735d34
Tanatorn Nateesanprasert <tanatorn@thinknet.co.th>
2021-05-08 15:26:01 +0700

    feat: experiment using gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import GlobalStyle from '@styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from '@styles/theme'
 import '@localized'
+import * as gtag from '@utils/gtag'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <GlobalStyle />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,17 @@
-import Document, { DocumentContext } from 'next/document'
+import NextDocument, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { GA_TRACKING_ID } from '@utils/gtag'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<{
-    styles: JSX.Element
-    html: string
-    head?: JSX.Element[]
-  }> {
+type Props = {
+  isProduction: boolean
+}
+class Document extends NextDocument<Props> {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
+    // Check if in production
+    const isProduction = process.env.NODE_ENV === 'production'
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -20,6 +20,7 @@ export default class MyDocument extends Document {
 
       const initialProps = await Document.getInitialProps(ctx)
       return {
+        isProduction,
         ...initialProps,
         styles: (
           <>
@@ -32,4 +33,39 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
+
+  render() {
+    const { isProduction } = this.props
+    return (
+      <Html lang="th">
+        <Head>
+          {isProduction && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default Document
